95c8c3a0091c1028c5198bf3cb3d10c7b5de0976
h22121 <h22121@lab.cpcc.edu>
2019-03-28 19:23:39 -0400

    WorkingWithFunctions2019032807:25pm

---
+++ app.js
@@ -0,0 +1,39 @@
+/*jslint esversion:6*/
+
+
+function add(a, b, ) {
+    return a + b;
+}
+
+const substract = function (a, b) {
+    return a - b;
+};
+
+const multiply = (a, b) => a * b;
+
+const log = (a) => console.log('value of a is : ${a}');
+
+const sumar = add;
+const AGE = 65;
+
+65;
+add;
+var value = add(3, 5)
+log;
+
+log("Hello World");
+
+AGE
+
+console.log(AGE);
+console.log(sumar('hello', 'world'));
+console.log(add);
+console.log(typeof add);
+console.log(substract(10, 7));
+console.log(substract);
+console.log(typeof multiply);
+console.log(multiply(3, 5));
+console.log(multiply);
+
+return;
+
