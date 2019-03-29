e0bd2b5cd12aaad76b0b3ba305bdf1fc28fa369c
h22121 <h22121@lab.cpcc.edu>
2019-03-28 21:02:54 -0400

    WorkingWithFuncations20190328 9:02pm

--- app2.js
+++ app2.js
@@ -1,16 +1,31 @@
+var name = "Other Bruce";
+
 const bruce = {
     name: "Bruce",
-    greet: function () {
-        return 'Hello ${this.name}';
+    bruceGreet: function () {
+        console.log(name);
+        this.name = "This.Bruce"
+        return `Hello ${this.name}`;
     }
 };
+
+console.log(bruce.bruceGreet());
+
+
+var bruceCopy = bruce;
+var x = 10;
+var y = x;
+y = 11;
+
+this.
+
+    bruceCopy.name = "BigBruce";
 
 console.log(bruce.greet());
 const madeline = { name: "Madeline" };
 function greet() {
-    return 'Hello ${this.name}';
+    return `Hello ${this.name}`;
 }
-
-madeline.greet = greet;
-console.log(madeline.greet());
+madeline.madelineGreet = greet;
+console.log(madeline.madelineGreet());
 console.log(greet());
